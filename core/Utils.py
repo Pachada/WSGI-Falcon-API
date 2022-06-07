aee168b75fa5dc2433434bf41cc835eb14a8877d
Pachada <dtrejog98@gmail.com>
2022-06-07 11:09:48 -0500

    logging module added

--- core/Utils.py
+++ core/Utils.py
@@ -8,7 +8,14 @@ import pytz
 import hashlib
 import os
 import base64
+import logging
 
+def timetz(*args):
+    return datetime.now(pytz.timezone("America/Mexico_City")).timetuple()
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - [ %(levelname)s ]: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+logging.Formatter.converter = timetz
+logger = logging.getLogger(__name__)
 
 class Utils:
     @staticmethod
